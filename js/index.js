452200a5dc4dfefd09e942c29f68496d997913d6
insistsu <947737976@qq.com>
2020-12-30 23:49:53 +0800

    y轴y轴线条不显示

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
   var myChart = echarts.init(document.querySelector('.bar .chart'));
   // 2.指定配置项
   var option = {
-    color: ['#3398DB'],
+    color: ['#2f89cf'],
     tooltip: {
         trigger: 'axis',
         axisPointer: {            // 坐标轴指示器，坐标轴触发有效
@@ -12,9 +12,10 @@
         }
     },
     grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
+        left: "0%",
+        top: "10px",
+        right: "0%",
+        bottom: "4%",
         containLabel: true
     },
     xAxis: [
@@ -23,12 +24,38 @@
             data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
             axisTick: {
                 alignWithLabel: true
+            },
+            // 修改刻度标签 相关样式
+            axisLabel: {
+              color: "rgba(255,255,255,.6)",
+              fontSize: "12"
+            },
+            // 不显示x坐标轴得样式
+            axisLine: {
+              show: false
             }
         }
     ],
     yAxis: [
         {
-            type: 'value'
+            type: 'value',
+            // y轴文字标签样式
+            axisLabel: {
+              color: "rgba(255,255,255,.6)",
+              fontSize: 12
+            },
+            // y轴改变了线得样式
+            axisLine: {
+              lineStyle: {
+                color: "rgba(255,255,255,.1)"
+              }
+            },
+            // y轴分割线得颜色
+            splitLine: {
+              lineStyle: {
+                color: "rgba(255,255,255,.1)"
+              }
+            }
         }
     ],
     series: [
